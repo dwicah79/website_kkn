ad35f3d36cce96e97c358bf7dfb52a8d86b41829
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-02-04 12:01:00 +0700

    add umkm migration

---
+++ database/migrations/2025_02_04_040230_create_umkms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('umkms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('thumbnail');
+            $table->string('telp');
+            $table->integer('jenis_umkm_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('umkms');
+    }
+};
